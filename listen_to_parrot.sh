c7de035b693c17dc415f9cabeac34bd36076b2cc
Robin Engler <robin.engler@sib.swiss>
2021-09-27 00:53:33 +0200

    Parrot remembers John Longsilver

--- listen_to_parrot.sh
+++ listen_to_parrot.sh
@@ -4,4 +4,17 @@ source .script/quest_functions.sh
 # Parrot welcome
 parrot_say "You're here for John Longsilver's treasure ?"
 sleep 1
+echo "Ah I miss John... such a good pirate..."
+echo ""
+sleep 1
 
+# Parrot remembers stuff...
+echo "The treasure... yes, I was with John when he was hiding it!"
+sleep 1
+
+echo "Back then he often spent time in the following locations:"
+echo " * key 1: Port-Royal, John Longsilver's ship is still docked there, visit the captain's cabin."
+echo " * key 2: Blackbird's castle, the west tower of the castle."
+echo " * key 3: Oyster bay, the old lighthouse, that was a pirate meeting place."
+echo " "
+sleep 2
