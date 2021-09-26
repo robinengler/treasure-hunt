b38ea7523f3f872a0cc979cc61023567a728be5a
Robin Engler <robin.engler@sib.swiss>
2021-09-26 17:55:38 +0200

    The junction

---
+++ .script/quest_functions.sh
@@ -0,0 +1,20 @@
+KEY_1="lock_1/key_1"
+KEY_2="lock_2/key_2"
+KEY_3="lock_3/key_3"
+CREW="pirate_code.md"
+PIRATES=( "Captain" "Quartermaster" "First mate" )
+
+function random_exclamation {
+    exclamations=( "Blimey!" "Sail Ho!" "Shiver me timbers!" "Sink me!" "Aaaarrrrgggghhhh!" "Blimey!" "Sail Ho!" "All Hand Hoy!" "Sink me!" "Scupper that!" )
+    echo ${exclamations[$(date | cut -f5 -d " " | cut -c 8)]}
+}
+
+function pirate_say {
+    cat .script/pirate.ascii
+    echo -e "$1"
+}
+
+function get_key_code {
+    key_nb=$1
+    cat .support_files/$(sha256sum "lock_${key_nb}/key_${key_nb}" | cut -c1-64) | cut -c$(($key_nb*2-1)),$(($key_nb*2+3))
+}
