fe7fc3e2d5776666e476c53c4cbac9d58dec79d4
Robin Engler <robin.engler@sib.swiss>
2021-09-27 00:46:50 +0200

    Update Longsilver's parrot speaks

--- listen_to_parrot.sh
+++ listen_to_parrot.sh
@@ -4,3 +4,9 @@ source .script/quest_functions.sh
 # Parrot welcome
 parrot_say "You're here for John Longsilver's treasure?"
 sleep 1
+
+# Parrot is not being helpful...
+for i in $( seq 1 10); do
+    echo "I don't remember anything..."
+    sleep 2
+done
