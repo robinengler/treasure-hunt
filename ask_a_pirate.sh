e172b6be12394480cb3c15380d3b25b2b81eca08
Robin Engler <robin.engler@sib.swiss>
2023-02-17 17:58:58 +0100

    Port-Royal downtown

--- ask_a_pirate.sh
+++ ask_a_pirate.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 source .script/quest_functions.sh
 
+# Quest starts here...
+pirate_say "Sail Ho! Welcome to Port-Royal, the pirate's city."
+echo -e "Here you will find anything you need... if you have enough gold!\n"
+sleep 3
+
+echo "Rhum? Yes, can do!"
+sleep 2
+echo "I heard they are storing it in the Tavern's backyard... "
+sleep 2
+echo -e "...maybe you could have a look there at night and steal some.\n"
+sleep 2
+
+echo -e "But don't forget: if you get caught... never mention me!\n"
+
+exit 0
