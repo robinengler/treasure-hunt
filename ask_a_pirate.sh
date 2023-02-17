bc56a7afb6a727dda6c17948a7efd3fd81e1ff48
Robin Engler <robin.engler@sib.swiss>
2023-02-17 17:58:13 +0100

    Castle main gate

--- ask_a_pirate.sh
+++ ask_a_pirate.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 source .script/quest_functions.sh
 
+# Quest starts here...
+pirate_say "Yes, you are at the famous Blackbird castle, home of legendary pirates."
+echo -e "For a silver coin I can show you around!\n"
+sleep 3
+
+echo "What?? You want to use Blackbird's flag?? Are you serious?"
+sleep 2
+echo "Oh well, if you're sure... let me see..."
+sleep 2
+echo "Check-out the old watch tower, but tread carefully..."
+sleep 2
+echo -e "And don't say I didn't warn you!\n"
+sleep 2
+
+echo -e "Now, how about that castle tour?\n"
+
+exit 0
